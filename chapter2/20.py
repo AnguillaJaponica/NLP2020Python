1f184c7492b2b18c88bbcc1c64510ed8bfd773b6
AnguillaJaponica <zoomzoomzoomtt4497@gmail.com>
2020-08-19 22:23:53 +0900

    add

---
+++ chapter2/20.py
@@ -0,0 +1,10 @@
+import json
+
+filename = 'jawiki-country.json'
+with open(filename, mode='r') as f:
+    for line in f:
+        line = json.loads(line)
+        if line['title'] == 'イギリス':
+            text_uk = line['text']
+            break
+print(text_uk)
