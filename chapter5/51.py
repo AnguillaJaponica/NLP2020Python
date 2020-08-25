0c35cc7f714b0c855c5a1fc5bb8f1c0763ca0f6f
AnguillaJaponica <zoomzoomzoomtt4497@gmail.com>
2020-08-25 20:36:16 +0900

    add

--- chapter5/51.py
+++ chapter5/51.py
@@ -1,3 +1,4 @@
+from sklearn.feature_extraction.text import TfidfVectorizer
 import string
 import re
 import pandas as pd
@@ -11,8 +12,40 @@ def preprocessing(text):
 
     return text
 
+
 df = pd.concat([train, valid, text], axis=0)
 df.reset_index(drop=True, inplace=True)
 
 df['TITLE'] = df['TITLE'].map(lambda x: preprocessing(x))
 print(df.head())
+
+
+# データの分割
+train_valid = df[:len(train) + len(valid)]
+test = df[len(train) + len(valid):]
+
+# TfidfVectorizer
+vec_tfidf = TfidfVectorizer(
+    min_df=10, ngram_range=(
+        1, 2))  # ngram_rangeでTF-IDFを計算する単語の長さを指定
+
+# ベクトル化
+X_train_valid = vec_tfidf.fit_transform(train_valid['TITLE'])  # testの情報は使わない
+X_test = vec_tfidf.transform(test['TITLE'])
+
+# ベクトルをデータフレームに変換
+X_train_valid = pd.DataFrame(
+    X_train_valid.toarray(),
+    columns=vec_tfidf.get_feature_names())
+X_test = pd.DataFrame(X_test.toarray(), columns=vec_tfidf.get_feature_names())
+
+# データの分割
+X_train = X_train_valid[:len(train)]
+X_valid = X_train_valid[len(train):]
+
+# データの保存
+X_train.to_csv('./X_train.txt', sep='\t', index=False)
+X_valid.to_csv('./X_valid.txt', sep='\t', index=False)
+X_test.to_csv('./X_test.txt', sep='\t', index=False)
+
+print(X_train.head())
