0db540495a01cad81872acdf7012f8aaa32b084d
AnguillaJaponica <zoomzoomzoomtt4497@gmail.com>
2020-08-25 21:01:25 +0900

    add

---
+++ chapter5/55.py
@@ -0,0 +1,14 @@
+from sklearn.metrics import confusion_matrix
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+# 学習データ
+train_cm = confusion_matrix(train['CATEGORY'], train_pred[1])
+print(train_cm)
+sns.heatmap(train_cm, annot=True, cmap='Blues')
+plt.show()
+
+test_cm = confusion_matrix(test['CATEGORY'], test_pred[1])
+print(test_cm)
+sns.heatmap(test_cm, annot=True, cmap='Blues')
+plt.show()
